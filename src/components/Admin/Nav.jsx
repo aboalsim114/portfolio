1174b7223d9b1ab0faf5def70cfa31af89e3fbce
aboalsim114 <aboalsim114@gmail.com>
2023-04-17 02:24:15 +0200

    update

--- src/components/Admin/Nav.jsx
+++ src/components/Admin/Nav.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { AiOutlineHome } from 'react-icons/ai';
 import { BiBook } from 'react-icons/bi';
 import { BiAddToQueue } from "react-icons/bi";
+import { BiLogOut } from "react-icons/bi";
 import { Link ,useParams,useNavigate} from 'react-router-dom';
 
 
@@ -14,14 +15,21 @@ const Nav = () => {
 
 
     const handleLogOut = (e) => {
-      
+      let auth = localStorage.getItem("token")
+
+      if(auth)
+      {
+        localStorage.clear()
+        window.location.reload();
+      }
+
     }
 
   return (
     <nav>
 <Link to={`/Dashboard/${userid}`} className={activeNav}><AiOutlineHome /></Link>
 <Link to={`/AddArticle/${userid}`} className={activeNav}><BiAddToQueue /></Link>
-<Link onClick={handleLogOut} className={activeNav}><BiAddToQueue /></Link>
+<Link onClick={handleLogOut} className={activeNav}><BiLogOut /></Link>
       
     </nav>
   )
